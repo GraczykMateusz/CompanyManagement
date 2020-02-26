91a5b767b6855fc5a3891535ca3068282025f584
wm <graczyk.mateusz@interia.pl>
2020-02-26 20:05:18 +0100

    add: company class

---
+++ Scripts/Company.py
@@ -0,0 +1,8 @@
+class Company:
+    def __init__(self, founder_name, founder_surname, company_name, company_address, tin, foundation_year):
+        self.__founder_name = founder_name
+        self.__founder_surname = founder_surname
+        self.__company_name = company_name
+        self.__company_address = company_address
+        self.__tin = tin
+        self.__foundation_year = foundation_year
