03c8a5ef8243bc381d10ace1ae0d6cb873c56593
wm <graczyk.mateusz@interia.pl>
2020-02-26 20:05:41 +0100

    add: employee class

---
+++ Scripts/Employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, name, surname, company_name, address, pesel, birthday):
+        self.__name = name
+        self.__surname = surname
+        self.__company_name = company_name
+        self.__address = address
+        self.__pesel = pesel
+        self.__birthday = birthday
