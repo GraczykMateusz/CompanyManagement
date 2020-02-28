3329e445bee80e416e28da45db0336efb6508c98
wm <graczyk.mateusz@interia.pl>
2020-02-28 10:20:02 +0100

    add: buttons event

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,21 +1,22 @@
 from Displayer import Displayer
+from Manager import Manager
 
 if __name__ == "__main__":
     window = Displayer()
     window.add_background("../Pictures/Background/background.png")
     window.add_window_icon("../Pictures/Icons/icon.png")
 
-    window.add_employee_button("../Pictures/Buttons/employee_add_button.png", 490)
-    window.add_employee_button("../Pictures/Buttons/employee_delete_button.png", 550)
-    window.add_employee_button("../Pictures/Buttons/employee_list_button.png", 610)
-    window.add_employee_button("../Pictures/Buttons/employee_find_button.png", 670)
+    window.add_employee_button("../Pictures/Buttons/employee_add_button.png", 490, Manager.add_employee)
+    window.add_employee_button("../Pictures/Buttons/employee_delete_button.png", 550, Manager.delete_employee)
+    window.add_employee_button("../Pictures/Buttons/employee_list_button.png", 610, Manager.list_employee)
+    window.add_employee_button("../Pictures/Buttons/employee_find_button.png", 670, Manager.find_employee)
 
-    window.add_company_button("../Pictures/Buttons/company_add_button.png", 490)
-    window.add_company_button("../Pictures/Buttons/company_delete_button.png", 550)
-    window.add_company_button("../Pictures/Buttons/company_list_button.png", 610)
-    window.add_company_button("../Pictures/Buttons/company_find_button.png", 670)
+    window.add_company_button("../Pictures/Buttons/company_add_button.png", 490, Manager.add_company)
+    window.add_company_button("../Pictures/Buttons/company_delete_button.png", 550, Manager.delete_company)
+    window.add_company_button("../Pictures/Buttons/company_list_button.png", 610, Manager.list_company)
+    window.add_company_button("../Pictures/Buttons/company_find_button.png", 670, Manager.find_company)
 
-    window.add_database_button("../Pictures/Buttons/database_download_button.png", 490)
-    window.add_database_button("../Pictures/Buttons/database_send_button.png", 550)
+    window.add_database_button("../Pictures/Buttons/database_download_button.png", 490, Manager.download_database)
+    window.add_database_button("../Pictures/Buttons/database_send_button.png", 550, Manager.send_database)
 
     window.mainloop()
