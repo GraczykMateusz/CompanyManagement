5ed4fcc7a4ff89d457d8b053892c86dbf3687039
wm <graczyk.mateusz@interia.pl>
2020-03-06 00:02:07 +0100

    refactor: tk.Tk as self

--- Scripts/StartPage.py
+++ Scripts/StartPage.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from Page import Page
 
-class StartPage(Page):
+class StartPage(tk.Tk, Page):
     
     def __init__(self, geometry="1200x800", background="../Pictures/Background/background.png", icon="../Pictures/Icons/icon.png"):
-        self.root = tk.Tk()
+        tk.Tk.__init__(self)
+
         self._set_window(geometry)
         self._add_background(background)
         self._add_window_icon(icon)
@@ -22,7 +23,7 @@ class StartPage(Page):
         position_x=220
 
         self.__employee_button_images_list.append(tk.PhotoImage(file = button_image))
-        self.__employee_button=tk.Button(self.root, command = lambda: event(), bd=0, highlightbackground='black', image = self.__employee_button_images_list[self.__employee_buttons_counter])
+        self.__employee_button=tk.Button(self, command = lambda: event(), bd=0, highlightbackground='black', image = self.__employee_button_images_list[self.__employee_buttons_counter])
         self.__employee_button.place(x = position_x, y = position_y)
         self.__employee_buttons_counter += 1
 
@@ -30,7 +31,7 @@ class StartPage(Page):
         position_x=520
 
         self.__company_button_images_list.append(tk.PhotoImage(file = button_image))
-        self.__company_button=tk.Button(self.root, command = lambda: event(), bd=0, highlightbackground='black', image = self.__company_button_images_list[self.__company_buttons_counter])
+        self.__company_button=tk.Button(self, command = lambda: event(), bd=0, highlightbackground='black', image = self.__company_button_images_list[self.__company_buttons_counter])
         self.__company_button.place(x = position_x, y = position_y)
         self.__company_buttons_counter += 1
 
@@ -38,7 +39,7 @@ class StartPage(Page):
         position_x=820
 
         self.__database_button_images_list.append(tk.PhotoImage(file=button_image))
-        self.__database_button=tk.Button(self.root, command = lambda: event(), bd=0, highlightbackground='black', image = self.__database_button_images_list[self.__database_buttons_counter])
+        self.__database_button=tk.Button(self, command = lambda: event(), bd=0, highlightbackground='black', image = self.__database_button_images_list[self.__database_buttons_counter])
         self.__database_button.place(x = position_x, y = position_y)
         self.__database_buttons_counter += 1
 
