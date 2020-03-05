dd1d6c98cd20976e46389e124f566b94c66a77ef
wm <graczyk.mateusz@interia.pl>
2020-03-05 23:41:56 +0100

    add: getters and changed ID variables

--- Scripts/Company.py
+++ Scripts/Company.py
@@ -1,8 +1,26 @@
 class Company:
-    def __init__(self, founder_name, founder_surname, company_name, company_address, tin, foundation_year):
+    def __init__(self, founder_name, founder_surname, company_name, company_address, tax_id, foundation_year):
         self.__founder_name = founder_name
         self.__founder_surname = founder_surname
         self.__company_name = company_name
         self.__company_address = company_address
-        self.__tin = tin
+        self.__tax_id = tax_id
         self.__foundation_year = foundation_year
+
+        def get_founder_name():
+            return self.__founder_name
+
+        def get_founder_surname():
+            return self.__founder_surname
+
+        def get_company_name():
+            return self.__company_name
+
+        def get_company_address():
+            return self.__company_address
+
+        def get_tax_id():
+            return self.__tax_id
+        
+        def get_foundation_year():
+            return self.__foundation_year
--- Scripts/Employee.py
+++ Scripts/Employee.py
@@ -1,8 +1,26 @@
 class Employee:
-    def __init__(self, name, surname, company_name, address, pesel, birthday):
+    def __init__(self, name, surname, company_name, address, personal_id, birthday):
         self.__name = name
         self.__surname = surname
         self.__company_name = company_name
         self.__address = address
-        self.__pesel = pesel
+        self.__personal_id = personal_id
         self.__birthday = birthday
+
+        def get_name():
+            return self.__name
+
+        def get_surname():
+            return self.__surname
+
+        def get_company_name():
+            return self.__company_name
+
+        def get_address():
+            return self.__address
+
+        def get_personal_id():
+            return self.__personal_id
+        
+        def get_birthday():
+            return self.__birthday
