ddfc6ae6859d6bcfe237cf7498e4eccc19ee4b58
wm <graczyk.mateusz@interia.pl>
2020-03-09 16:35:36 +0100

    delete: set/add sub background

--- Scripts/Page.py
+++ Scripts/Page.py
@@ -7,17 +7,7 @@ class Page:
         self.resizable(width=False, height=False)
         self.title("CompanyManagment by Mateusz Graczyk")
 
-    def _set_sub_window(self, geometry):
-        self.geometry(geometry)
-        self.resizable(width=False, height=False)
-        self.title("CompanyManagment by Mateusz Graczyk")
-
     def _add_background(self, path_to_image):
-        self.__background_image = tk.PhotoImage(file = path_to_image)
-        self.__background = tk.Label(self, image = self.__background_image)
-        self.__background.place(x=-1, y=-1)
-
-    def _add_sub_background(self, path_to_image):
         self.__background_image = tk.PhotoImage(file = path_to_image)
         self.__background = tk.Label(self, image = self.__background_image)
         self.__background.place(x=-1, y=-1)
