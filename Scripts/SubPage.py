abec26ae27c878874e38b56d4a91a7bc52383d4d
wm <graczyk.mateusz@interia.pl>
2020-03-09 22:05:05 +0100

    add: entry and tip checks (sketch)

--- Scripts/SubPage.py
+++ Scripts/SubPage.py
@@ -1,6 +1,7 @@
 from Company import Company
 from Employee import Employee
 from Page import Page
+from StartPage import StartPage
 
 import tkinter as tk
 
@@ -22,15 +23,25 @@ class SubPage(Page):
 
         self.__company_list = []
         self.__employee_list = []
+        self.__entry_list = []
 
     def add_company(self):
         self.__window_add_company = self.check_window_existence(self.__window_add_company)
 
-        self.window_config(self.__window_add_company, "../Pictures/Background/employee_add_background_tip.png")
+        if StartPage.tip.get() == True:
+            self.window_config(self.__window_add_company, "../Pictures/Background/employee_add_background_tip.png", "../Pictures/Icons/company_add_image.png")
+        else:
+            self.window_config(self.__window_add_company, "../Pictures/Background/employee_add_background.png", "../Pictures/Icons/company_add_image.png")
 
-        self.entry = tk.Entry(self.__window_add_company)
-        self.entry.place(x=370,y=200)
-        
+        self.add_entry(277)
+        self.add_entry(366)
+        self.add_entry(453)
+        self.add_entry(538)
+        self.add_entry(630)
+
+        self.add_entry(277, 840)
+        self.add_entry(366, 840)
+
     def delete_company(self):
         self.__window_delete_company = self.check_window_existence(self.__window_delete_company)
     
@@ -58,12 +69,21 @@ class SubPage(Page):
     def download_database(self):
         print('TEST 10')
 
+    def check_window_tip(self):
+        pass
+
     def check_window_existence(self, top):
         if top is not None:
             top.destroy()
         top = tk.Toplevel()
         return top
 
-    def window_config(self, top, background):
+    def window_config(self, top, background, path_to_image):
         SubPage._set_window(top, self.__geometry)
+        SubPage._add_window_icon(top, path_to_image)
         SubPage._add_background(top, background)
+
+    def add_entry(self, position_y, position_x=485):
+        entry = tk.Entry(self.__window_add_company, justify='center', bd=4, width=18, bg='grey', font = ('DejaVu Serif', 13, 'bold'))
+        entry.place(x = position_x, y = position_y)
+        self.__entry_list.append(entry)
