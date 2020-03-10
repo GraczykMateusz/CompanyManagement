b15138d922ce88646180c4c9a1847e722b604ce2
wm <graczyk.mateusz@interia.pl>
2020-03-10 21:40:58 +0100

    refactor: add buttons, sub_window object and changed tip given event

--- Scripts/StartPage.py
+++ Scripts/StartPage.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from Page import Page
+from SubPage import SubPage
 
 class StartPage(tk.Tk, Page):
     
@@ -8,10 +9,8 @@ class StartPage(tk.Tk, Page):
     def __init__(self, geometry="1200x800", background="../Pictures/Background/background.png", icon="../Pictures/Icons/icon.png"):
         tk.Tk.__init__(self)
 
-        self._set_window(geometry)
-        self._add_background(background)
-        self._add_window_icon(icon)
-    
+        self.sub_window = SubPage()
+
         self.__employee_button_images_list = []
         self.__employee_buttons_counter=0
 
@@ -19,9 +18,28 @@ class StartPage(tk.Tk, Page):
         self.__company_buttons_counter=0
 
         self.__database_button_images_list = []
-        self.__database_buttons_counter=0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        self.__database_buttons_counter=0
 
-    def add_employee_button(self, button_image, position_y, event):
+        self._set_window(geometry)
+        self._add_background(background)
+        self._add_window_icon(icon)
+
+        self.add_employee_button("../Pictures/Buttons/employee_add_button.png", 490, self.sub_window.add_employee)
+        self.add_employee_button("../Pictures/Buttons/employee_delete_button.png", 550, self.sub_window.delete_employee)
+        self.add_employee_button("../Pictures/Buttons/employee_list_button.png", 610, self.sub_window.list_employee)
+        self.add_employee_button("../Pictures/Buttons/employee_find_button.png", 670, self.sub_window.find_employee)
+
+        self.add_company_button("../Pictures/Buttons/company_add_button.png", 490, self.sub_window.add_company)
+        self.add_company_button("../Pictures/Buttons/company_delete_button.png", 550, self.sub_window.delete_company)
+        self.add_company_button("../Pictures/Buttons/company_list_button.png", 610, self.sub_window.list_company)
+        self.add_company_button("../Pictures/Buttons/company_find_button.png", 670, self.sub_window.find_company)
+
+        self.add_database_button("../Pictures/Buttons/database_download_button.png", 490, self.sub_window.download_database)
+        self.add_database_button("../Pictures/Buttons/database_send_button.png", 550, self.sub_window.send_database)
+
+        self.radio_tip_button()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+    def add_employee_button(self, button_image, position_y, event=None):
         position_x=220
 
         self.__employee_button_images_list.append(tk.PhotoImage(file = button_image))
@@ -29,7 +47,7 @@ class StartPage(tk.Tk, Page):
         self.__employee_button.place(x = position_x, y = position_y)
         self.__employee_buttons_counter += 1
 
-    def add_company_button(self, button_image, position_y, event):
+    def add_company_button(self, button_image, position_y, event=None):
         position_x=520
 
         self.__company_button_images_list.append(tk.PhotoImage(file = button_image))
@@ -37,7 +55,7 @@ class StartPage(tk.Tk, Page):
         self.__company_button.place(x = position_x, y = position_y)
         self.__company_buttons_counter += 1
 
-    def add_database_button(self, button_image, position_y, event):
+    def add_database_button(self, button_image, position_y, event=None):
         position_x=820
 
         self.__database_button_images_list.append(tk.PhotoImage(file=button_image))
@@ -49,8 +67,8 @@ class StartPage(tk.Tk, Page):
         StartPage.tip = tk.BooleanVar() 
         StartPage.tip.set("True")
         
-        self.__tip_button_on = tk.Radiobutton(self, width="4", bd=0, text="ON ", bg='grey', variable=StartPage.tip, value=True)
-        self.__tip_button_off = tk.Radiobutton(self, width="4", bd=0, text="OFF", bg='grey', variable=StartPage.tip, value=False)
+        self.__tip_button_on = tk.Radiobutton(self, width="4", bd=0, text="ON ", bg='grey', variable=StartPage.tip, value=True, command = lambda: self.sub_window.check_window_tip(StartPage.tip.get()))
+        self.__tip_button_off = tk.Radiobutton(self, width="4", bd=0, text="OFF", bg='grey', variable=StartPage.tip, value=False, command = lambda: self.sub_window.check_window_tip(StartPage.tip.get()))
         
         self.__tip_button_on.place(x=1105, y=710)
         self.__tip_button_off.place(x=1105, y=731)
