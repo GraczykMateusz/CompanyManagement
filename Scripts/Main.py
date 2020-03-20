105df6c1fba5023084a721fea469a2f6f1d4a7a4
wm <graczyk.mateusz@interia.pl>
2020-03-20 13:52:20 +0100

    add: import employees/companies data from file

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,6 +1,9 @@
+from CompanyManagment import CompanyManagmet
 from StartPage import StartPage
 
 if __name__ == "__main__":
     
+    CompanyManagmet.import_data()
+
     window = StartPage()
     window.mainloop()
